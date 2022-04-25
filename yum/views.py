2ef32e8f7664dd9006f5abb5ffac3da74ecbb503
Sprentiss <sprentis@purdeu.edu>
2022-04-25 18:25:42 -0400

    rename trigger file

--- yum/views.py
+++ yum/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import *
 from .forms import *
-from .Triggers import *
+from .triggers import *
 from .filters import *
 
 # Create your views here.
